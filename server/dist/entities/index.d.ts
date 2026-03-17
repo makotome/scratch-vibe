3cf2fcef0859cfd8b6e1dfba7a68421f4672fdd1
xumx <makotome@126.com>
2026-03-17 16:20:05 +0800

    Add .gitignore and remove cached build files

--- server/dist/entities/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './user.entity';
-import { Project } from './project.entity';
-export * from './user.entity';
-export * from './project.entity';
-export declare const entities: (typeof Project | typeof User)[];
--- server/dist/modules/user/user.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    findAll(): Promise<import("../../entities").User[]>;
-    findOne(id: string): Promise<import("../../entities").User>;
-}
